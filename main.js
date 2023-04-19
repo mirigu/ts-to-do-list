cf96997fc5f99e8e598b9caf025f52406877aefc
mirigu <miriggu9@gmail.com>
2023-04-19 21:13:12 +0900

    feat: 할 일 추가 및 완료 기능 구현

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var _a;
 var tabItem = (document.querySelectorAll('.tab-item'));
 var tabContent = (document.querySelectorAll('.tab-content'));
 var form = document.querySelector('.form-container');
@@ -7,6 +8,7 @@ var todoList = document.querySelector('.todo-list');
 var completedList = document.querySelector('.completed-list');
 var deletedList = document.querySelector('.deleted-list');
 var completeDeletedBtn = (document.querySelector('.completed-delete-button'));
+var toDoList = JSON.parse((_a = localStorage.getItem('TODO')) !== null && _a !== void 0 ? _a : '[]');
 // 탭 클릭시 실행되는 함수
 var handleTabClick = function (event) {
     var _a;
@@ -21,6 +23,90 @@ var handleTabClick = function (event) {
     (_a = document.querySelector("#".concat(target))) === null || _a === void 0 ? void 0 : _a.classList.add('target');
     tabTarget.classList.add('active');
 };
+// Date를 YYYY-MM-dd HH:mm:ss 으로 변환하는 함수
+var dateFormat = function (date) {
+    var prependZero = function (num) {
+        // 숫자가 한자릿수인 경우, 앞자리에 0을 붙이는 함수
+        if (num < 10) {
+            return "0".concat(num);
+        }
+        return num;
+    };
+    var _a = [
+        // 연도, 월, 일
+        date.getFullYear(),
+        prependZero(date.getMonth() + 1),
+        prependZero(date.getDay()),
+    ], year = _a[0], month = _a[1], day = _a[2];
+    var _b = [
+        // 시, 분, 초
+        prependZero(date.getHours()),
+        prependZero(date.getMinutes()),
+        prependZero(date.getSeconds()),
+    ], hour = _b[0], minutes = _b[1], seconds = _b[2];
+    return "".concat(year, "-").concat(month, "-").concat(day, " ").concat(hour, ":").concat(minutes, ":").concat(seconds);
+};
+// 체크박스 클릭시 실행되는 함수
+var handleCheckChange = function (id) {
+    toDoList.filter(function (todo) {
+        if (todo.id === Number(id)) {
+            return (todo.completed = !todo.completed);
+        }
+    });
+    saveTodo();
+    addTodoList();
+};
+// 로컬스토리지에 할 일을 저장하는 함수
+var saveTodo = function () {
+    localStorage.setItem('TODO', JSON.stringify(toDoList));
+};
+var createTodo = function (newTodo) {
+    var todo = document.createElement('div');
+    var content = "\n    <input class='checkbox' type='checkbox' ".concat(newTodo.completed ? 'checked' : '', " />\n    <span class='text' style='background-color:").concat(newTodo.label, ";'>").concat(newTodo.text, "</span>\n    <span>(\uB4F1\uB85D) ").concat(newTodo.createAt, "</span>\n    ").concat(newTodo.update ? "<span>(\uC218\uC815) ".concat(newTodo.updateAt, "</span>") : '', "\n    <button class='update-button'>\uC218\uC815</button>\n    <button class='delete-button'>\uC0AD\uC81C</button>\n  ");
+    todo.className = 'content';
+    todo.innerHTML = content;
+    todo.id = newTodo.id.toString();
+    todo.querySelectorAll('input').forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () { return handleCheckChange(todo.id); });
+    });
+    return todo;
+};
+var addTodoList = function () {
+    var incomplete = toDoList.filter(function (item) { return !item.completed; });
+    var completed = toDoList.filter(function (item) { return item.completed; });
+    todoList.innerHTML = '';
+    incomplete.forEach(function (item) {
+        var todo = createTodo(item);
+        todoList.appendChild(todo);
+    });
+    completedList.innerHTML = '';
+    completed.forEach(function (item) {
+        var todo = createTodo(item);
+        completedList.appendChild(todo);
+    });
+};
+// 추가 버튼 클릭시 실행되는 함수
+var handleSubmit = function (event) {
+    event.preventDefault();
+    if (formInput.value.length < 1 || formInput.value.length > 20) {
+        return alert('글자수를 확인해주세요.');
+    }
+    var newTodo = {
+        id: toDoList.length + 1,
+        text: formInput.value,
+        label: formLabel.value,
+        createAt: dateFormat(new Date()),
+        completed: false,
+        update: false,
+    };
+    toDoList.push(newTodo);
+    saveTodo();
+    addTodoList();
+    formInput.value = '';
+    formLabel.value = '#000000';
+};
 tabItem.forEach(function (tab) {
     tab.addEventListener('click', handleTabClick);
 });
+form.addEventListener('submit', handleSubmit);
+window.addEventListener('DOMContentLoaded', addTodoList);
--- main.ts
+++ main.ts
@@ -17,6 +17,18 @@ const completeDeletedBtn = <HTMLDivElement>(
   document.querySelector('.completed-delete-button')
 );
 
+interface Todo {
+  id: number;
+  text: string;
+  label: string;
+  createAt: string;
+  completed: boolean;
+  update: boolean;
+  updateAt?: string;
+}
+
+let toDoList: Todo[] = JSON.parse(localStorage.getItem('TODO') ?? '[]');
+
 // 탭 클릭시 실행되는 함수
 const handleTabClick = (event: MouseEvent): void => {
   const tabTarget = <HTMLLIElement>event.target;
@@ -35,6 +47,125 @@ const handleTabClick = (event: MouseEvent): void => {
   tabTarget.classList.add('active');
 };
 
+// Date를 YYYY-MM-dd HH:mm:ss 으로 변환하는 함수
+const dateFormat = (date: Date): string => {
+  const prependZero = (num: number): number | string => {
+    // 숫자가 한자릿수인 경우, 앞자리에 0을 붙이는 함수
+    if (num < 10) {
+      return `0${num}`;
+    }
+
+    return num;
+  };
+
+  const [year, month, day] = [
+    // 연도, 월, 일
+    date.getFullYear(),
+    prependZero(date.getMonth() + 1),
+    prependZero(date.getDay()),
+  ];
+
+  const [hour, minutes, seconds] = [
+    // 시, 분, 초
+    prependZero(date.getHours()),
+    prependZero(date.getMinutes()),
+    prependZero(date.getSeconds()),
+  ];
+
+  return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+};
+
+// 체크박스 클릭시 실행되는 함수
+const handleCheckChange = (id: string): void => {
+  toDoList.filter((todo: Todo) => {
+    if (todo.id === Number(id)) {
+      return (todo.completed = !todo.completed);
+    }
+  });
+
+  saveTodo();
+  addTodoList();
+};
+
+// 로컬스토리지에 할 일을 저장하는 함수
+const saveTodo = () => {
+  localStorage.setItem('TODO', JSON.stringify(toDoList));
+};
+
+const createTodo = (newTodo: Todo): HTMLDivElement => {
+  let todo = <HTMLDivElement>document.createElement('div');
+
+  const content: string = `
+    <input class='checkbox' type='checkbox' ${
+      newTodo.completed ? 'checked' : ''
+    } />
+    <span class='text' style='background-color:${newTodo.label};'>${
+    newTodo.text
+  }</span>
+    <span>(등록) ${newTodo.createAt}</span>
+    ${newTodo.update ? `<span>(수정) ${newTodo.updateAt}</span>` : ''}
+    <button class='update-button'>수정</button>
+    <button class='delete-button'>삭제</button>
+  `;
+
+  todo.className = 'content';
+  todo.innerHTML = content;
+  todo.id = newTodo.id.toString();
+
+  todo.querySelectorAll('input').forEach((checkbox: HTMLInputElement): void => {
+    checkbox.addEventListener('change', () => handleCheckChange(todo.id));
+  });
+
+  return todo;
+};
+
+const addTodoList = () => {
+  const incomplete = toDoList.filter((item) => !item.completed);
+  const completed = toDoList.filter((item) => item.completed);
+
+  todoList.innerHTML = '';
+  incomplete.forEach((item) => {
+    const todo = createTodo(item);
+    todoList.appendChild(todo);
+  });
+
+  completedList.innerHTML = '';
+  completed.forEach((item) => {
+    const todo = createTodo(item);
+    completedList.appendChild(todo);
+  });
+};
+
+// 추가 버튼 클릭시 실행되는 함수
+const handleSubmit = (event: SubmitEvent): void => {
+  event.preventDefault();
+
+  if (formInput.value.length < 1 || formInput.value.length > 20) {
+    return alert('글자수를 확인해주세요.');
+  }
+
+  const newTodo: Todo = {
+    id: toDoList.length + 1,
+    text: formInput.value,
+    label: formLabel.value,
+    createAt: dateFormat(new Date()),
+    completed: false,
+    update: false,
+  };
+
+  toDoList.push(newTodo);
+
+  saveTodo();
+  addTodoList();
+
+  formInput.value = '';
+  formLabel.value = '#000000';
+};
+
 tabItem.forEach((tab: HTMLLIElement): void => {
   tab.addEventListener('click', handleTabClick);
 });
+
+form.addEventListener('submit', handleSubmit);
+
+window.addEventListener('DOMContentLoaded', addTodoList);
